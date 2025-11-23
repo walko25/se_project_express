045686d729360c2df13681ca6545b16e8e413529
Walker Owen <walkowen@gmail.com>
2025-11-23 14:35:16 -0800

    12-6

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
-// const routes = require("./routes");
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use(express.json());
-// app.use(routes);
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
